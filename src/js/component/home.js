8f17e9bd7b325d99c251cadcf8f6c0e7e3362838
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2020-12-03 17:36:25 -0400

    Inicia hooks

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	// state, setState
+	// peras, guardarPeras
+	const [anotherState, setAnotherState] = useState(0);
+	const [age, setAge] = useState(35);
+	const [isSuccess, setIsSuccess] = useState(true);
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
+			<a
+				href="#"
+				className={`btn ${
+					isSuccess == true ? "btn-success" : "btn-primary"
+				}`}
+				onClick={e => setIsSuccess(!isSuccess)}>
+				{"cambiar color"}
 			</a>
 			<p>
 				Made by{" "}
